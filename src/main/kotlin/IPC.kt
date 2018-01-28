dca15aa6f0d2758a5bea1ddc650017be483480ff
DavidMellul <david.mellul@outlook.fr>
2018-01-29 00:19:44 +0100

    First commit! Please work

---
+++ src/main/kotlin/IPC.kt
@@ -0,0 +1,29 @@
+private object IPC {
+    val handles: HashMap<Any, HashMap<String, (Any?) -> Unit>> = HashMap()
+}
+
+data class SignalAssociation(val receiver: Any, val signal: String)
+data class SophisticatedSignal(val name: String, val parameters: Any?)
+
+
+// Event broadcasting
+infix fun Any.broadcast(signal: String) = IPC.handles.filter { (_, h) -> h.contains(signal) }.forEach { (r, _) -> r consume signal }
+
+infix fun Any.broadcast(signal: SophisticatedSignal) = IPC.handles.filter { (_, h) -> h.contains(signal.name) }.forEach { (r, _) -> r consume signal }
+
+// Event consumption
+infix fun Any.consume(signal: String) = IPC.handles[this]?.get(signal)?.invoke(null)
+
+infix fun Any.consume(signal: SophisticatedSignal) = IPC.handles[this]?.get(signal.name)?.invoke(signal.parameters)
+
+
+// Event listen & stop listen
+infix fun Any.listenTo(signal: String): SignalAssociation {
+    if (IPC.handles[this] == null)
+        IPC.handles[this] = HashMap()
+    return SignalAssociation(this, signal)
+}
+
+infix fun SignalAssociation.then(block: (Any?) -> Unit) = IPC.handles[receiver]?.put(signal, block)
+infix fun Any.stopListenTo(signal: String) = IPC.handles[this]?.remove(signal)
+
